492bf5ee7f813907edd8959f75da8e75d427f15a
SAI TEJ <saitej10102008@gmail.com>
2026-09-20 22:34:18 +0530

    Implement function to check for duplicate values

---
+++ ContainsDuplicate.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+bool containsDuplicate(int* nums, int numsSize) {
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+
+            if (nums[i] == nums[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
